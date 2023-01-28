dea2bd60b0064f3125449b4e8690de79e632a393
Lorenzo <44714920+lorenzofavaro@users.noreply.github.com>
2023-01-28 01:26:55 +0100

    language change scripts

--- script/ita_to_en.py
+++ script/ita_to_en.py
@@ -1,7 +1,7 @@
 with open('../music.owl', encoding='utf-8') as f:
     text = f.read()
 
-text = text.replace('@ita', '@it')
+text = text.replace('@it', '@ita')
 print(text)
 with open('../music.owl', 'w', encoding='utf-8') as f:
     f.write(text)
