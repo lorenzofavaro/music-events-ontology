012c6ca7a26237465acb194f80cd137818e54c1c
Lorenzo <44714920+lorenzofavaro@users.noreply.github.com>
2023-02-02 17:54:40 +0100

    added dataframe filters

--- web-app/pages/🎵 Songs.py
+++ web-app/pages/🎵 Songs.py
@@ -3,10 +3,21 @@ import streamlit as st
 from init import *
 
 songs_query = onto_prefix + queries['list']['songs']
+filters = ['Song', 'Composer', 'Album']
 
 st.markdown(hide_st_style, unsafe_allow_html=True)
 st.title('Songs 🎵')
 result = g.query(songs_query)
 
-df = beautify_df(pd.DataFrame(result, columns=result.vars))
-st.dataframe(df, use_container_width=True)
+col1, col2 = st.columns(2)
+
+with col1:
+    search = st.text_input('To search', placeholder='Hey Jude').lower()
+with col2:
+    filter = st.selectbox('Filter', filters)
+df = pd.DataFrame(result, columns=result.vars)
+
+if search:
+    df = df[df.iloc[:, filters.index(filter)].str.lower().str.contains(search)]
+
+st.dataframe(beautify_df(df), use_container_width=True)
--- web-app/pages/💿 Albums.py
+++ web-app/pages/💿 Albums.py
@@ -3,10 +3,22 @@ import streamlit as st
 from init import *
 
 albums_query = onto_prefix + queries['list']['albums']
+filters = ['Album', 'Owner']
 
 st.markdown(hide_st_style, unsafe_allow_html=True)
 st.title('Albums 💿')
 result = g.query(albums_query)
 
-df = beautify_df(pd.DataFrame(result, columns=result.vars))
-st.dataframe(df, use_container_width=True)
+col1, col2 = st.columns(2)
+
+with col1:
+    search = st.text_input('To search', placeholder='Talking Book').lower()
+with col2:
+    filter = st.selectbox('Filter', filters)
+df = pd.DataFrame(result, columns=result.vars)
+
+if search:
+    df = df[df.iloc[:, filters.index(filter)].str.lower().str.contains(search)]
+
+st.dataframe(beautify_df(df), use_container_width=True)
+
