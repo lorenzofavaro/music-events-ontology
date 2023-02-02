fba5ab2764660140cc93a36c059a86bfd3a3467e
Lorenzo <44714920+lorenzofavaro@users.noreply.github.com>
2023-02-02 17:42:41 +0100

    deleted scripts

--- web-app/scripts/en_to_ita.py
+++
@@ -1,9 +0,0 @@
-ontology_path = '../res/music.rdf'
-
-with open(ontology_path, encoding='utf-8') as f:
-    text = f.read()
-
-text = text.replace('xml:lang="ita"', 'xml:lang="it"')
-
-with open(ontology_path, 'w', encoding='utf-8') as f:
-    f.write(text)
--- web-app/scripts/ita_to_en.py
+++
@@ -1,9 +0,0 @@
-ontology_path = '../res/music.rdf'
-
-with open(ontology_path, encoding='utf-8') as f:
-    text = f.read()
-
-text = text.replace('xml:lang="it"', 'xml:lang="ita"')
-
-with open(ontology_path, 'w', encoding='utf-8') as f:
-    f.write(text)
