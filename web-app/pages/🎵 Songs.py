d6a1385c7b79c170112befbe3d83da07dc3a4c87
Lorenzo <44714920+lorenzofavaro@users.noreply.github.com>
2023-02-03 12:26:20 +0100

    filtered songs & albums

--- web-app/pages/🎵 Songs.py
+++ web-app/pages/🎵 Songs.py
@@ -2,22 +2,29 @@ import streamlit as st
 
 from init import *
 
-songs_query = onto_prefix + queries['list']['songs']
-filters = ['Song', 'Composer', 'Album']
+
+def filtered_query(genre):
+    filtered_songs_query = onto_prefix + queries['list_filter']['songs']
+    filter = f"REGEX(?Genre, '^{genre if genre else '[A-Z]'}*', 'i')"
+    return filtered_songs_query.format(filter)
+
 
 st.markdown(hide_st_style, unsafe_allow_html=True)
+category_filters = ['Song', 'Composer', 'Album']
+genre_filters = ['', 'Pop', 'Rock', 'Folk', 'Jazz']
 st.title('Songs 🎵')
-result = g.query(songs_query)
 
+genre = st.selectbox('Genre', genre_filters)
 col1, col2 = st.columns(2)
-
 with col1:
     search = st.text_input('To search', placeholder='Hey Jude').lower()
 with col2:
-    filter = st.selectbox('Filter', filters)
+    category = st.selectbox('Category', category_filters)
+
+result = g.query(filtered_query(genre))
 df = pd.DataFrame(result, columns=result.vars)
 
 if search:
-    df = df[df.iloc[:, filters.index(filter)].str.lower().str.contains(search)]
+    df = df[df.iloc[:, category_filters.index(category)].str.lower().str.contains(search)]
 
 st.dataframe(beautify_df(df), use_container_width=True)
--- web-app/pages/💿 Albums.py
+++ web-app/pages/💿 Albums.py
@@ -2,23 +2,29 @@ import streamlit as st
 
 from init import *
 
-albums_query = onto_prefix + queries['list']['albums']
-filters = ['Album', 'Owner']
+
+def filtered_query(certification):
+    filtered_albums_query = onto_prefix + queries['list_filter']['albums']
+    filter = f"REGEX(?Certification, '^{certification if certification else '[A-Z]'}*', 'i')"
+    return filtered_albums_query.format(filter)
+
 
 st.markdown(hide_st_style, unsafe_allow_html=True)
+category_filters = ['Album', 'Owner']
+certification_filters = ['', 'Gold Certification', 'Platinum Certification', 'Diamond Certification']
 st.title('Albums 💿')
-result = g.query(albums_query)
 
+certification = st.selectbox('Certification', certification_filters)
 col1, col2 = st.columns(2)
-
 with col1:
     search = st.text_input('To search', placeholder='Talking Book').lower()
 with col2:
-    filter = st.selectbox('Filter', filters)
+    filter = st.selectbox('Category', category_filters)
+
+result = g.query(filtered_query(certification))
 df = pd.DataFrame(result, columns=result.vars)
 
 if search:
     df = df[df.iloc[:, filters.index(filter)].str.lower().str.contains(search)]
 
 st.dataframe(beautify_df(df), use_container_width=True)
-
